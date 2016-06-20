1c71ab67505b625135933d8dadb379b2613d0fcf
Marcus Vinicius <vmvini@hotmail.com>
2016-06-20 08:45:35 -0300

    DAO de Produto implementado

--- SrEstoquePersistencia/src/main/java/br/edu/ifpb/praticas/srestoque/srestoquepersistencia/GerenciadorProdutoImpl.java
+++ SrEstoquePersistencia/src/main/java/br/edu/ifpb/praticas/srestoque/srestoquepersistencia/GerenciadorProdutoImpl.java
@@ -5,11 +5,15 @@
  */
 package br.edu.ifpb.praticas.srestoque.srestoquepersistencia;
 
+import br.edu.ifpb.praticas.srestoque.exceptions.NenhumProdutoCadastrado;
+import br.edu.ifpb.praticas.srestoque.exceptions.ProdutoNaoEncontrado;
 import br.edu.ifpb.praticas.srestoque.srestoqueentidades.Produto;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  *
@@ -36,44 +40,67 @@ public class GerenciadorProdutoImpl implements GerenciadorProduto {
     }
 
     @Override
-    public void atualizarProduto(Produto produto) {
+    public void atualizarProduto(Produto produto) throws ProdutoNaoEncontrado {
         em.getTransaction().begin();
-        if(!em.contains(produto)){
+        if(em.contains(produto)){
             em.refresh(produto);
             em.flush();
         }
+        else
+            throw new ProdutoNaoEncontrado("Produto " + produto.getDescricao() + " id: " + produto.getId() + " nao existe");
         em.getTransaction().commit();
     
     }
 
     @Override
-    public void removerProduto(Produto produto) {
+    public void removerProduto(Produto produto) throws ProdutoNaoEncontrado{
         em.getTransaction().begin();
-        if(!em.contains(produto)){
+        if(em.contains(produto)){
             em.remove(produto);
             em.flush();
         }
+        else
+            throw new ProdutoNaoEncontrado("Produto " + produto.getDescricao() + " id: " + produto.getId() + " não encontrado");
         em.getTransaction().commit();
     
     }
 
     @Override
-    public Produto buscarPorId(int id) {
-        em.getTransaction().begin();
-        Produto produto = em.getReference(Produto.class, id);
-        em.getTransaction().commit();
-        return produto;
+    public Produto buscarPorId(int id) throws ProdutoNaoEncontrado {
+        try{
+            em.getTransaction().begin();
+            Produto produto = em.getReference(Produto.class, id);
+            em.getTransaction().commit();
+            return produto;
+        }
+        catch(EntityNotFoundException e){
+            throw new ProdutoNaoEncontrado("Produto de id " + id + " não encontrado");
+        }
+        
+        
     
     }
 
     @Override
-    public List<Produto> buscarPorDescricao(String descricao) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Produto> buscarPorDescricao(String descricao) throws ProdutoNaoEncontrado{
+        
+        Query query = em.createQuery("SELECT p FROM produto p WHERE p.descricao LIKE CONCAT('%', ?1, '%')");
+        query.setParameter(1, descricao);
+        List<Produto> produtos = query.getResultList();
+        if(produtos.isEmpty())
+            throw new ProdutoNaoEncontrado("Produto de descricao '" + descricao + "' não encontrado");
+        return produtos;
+    
     }
 
     @Override
-    public List<Produto> listarProdutos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Produto> listarProdutos() throws NenhumProdutoCadastrado {
+            
+        Query query = em.createQuery("SELECT p FROM produto p");
+        List<Produto> produtos = query.getResultList();
+        if(produtos.isEmpty())
+            throw new NenhumProdutoCadastrado("Não existe nenhum produto cadastrado.");
+        return produtos;
     }
     
 }
