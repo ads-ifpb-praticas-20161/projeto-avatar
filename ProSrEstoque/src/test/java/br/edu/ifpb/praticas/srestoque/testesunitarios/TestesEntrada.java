03e258734ab1d9960bbdecc73d41ce17dea7d7a0
Marcus Vinicius <vmvini@hotmail.com>
2016-07-26 21:20:14 -0300

    estrutura de testes unitarios

---
+++ ProSrEstoque/src/test/java/br/edu/ifpb/praticas/srestoque/testesunitarios/TestesEntrada.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.praticas.srestoque.testesunitarios;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author vmvini
+ */
+
+
+public class TestesEntrada {
+
+    
+    
+    
+    
+}
