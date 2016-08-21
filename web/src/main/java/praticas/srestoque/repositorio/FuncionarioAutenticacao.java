bb5e9cfbc6a46d6ea0d678b28e03a1a34d3fd60c
Marcus Vinicius <vmvini@hotmail.com>
2016-08-21 01:23:59 -0300

    classe de autenticacao de funcionario ouvindo NoResultException

--- web/src/main/java/praticas/srestoque/repositorio/FuncionarioAutenticacao.java
+++ web/src/main/java/praticas/srestoque/repositorio/FuncionarioAutenticacao.java
@@ -7,10 +7,11 @@ package praticas.srestoque.repositorio;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import praticas.srestoque.entidades.Funcionario;
+import javax.naming.AuthenticationException;
 
 /**
  *
@@ -22,11 +23,17 @@ public class FuncionarioAutenticacao {
     @PersistenceContext
     private EntityManager em;
     
-    public Funcionario login(String login, String senha){
-        TypedQuery<Funcionario> query = em.createQuery("SELECT f FROM Funcionario f WHERE f.login =: login AND f.senha =: senha", Funcionario.class);
-        query.setParameter("login", login);
-        query.setParameter("senha", senha);
-        return query.getSingleResult();
+    public Funcionario login(String email, String senha) throws AuthenticationException{
+        try{
+            TypedQuery<Funcionario> query = em.createQuery("SELECT f FROM Funcionario f WHERE f.email =:email AND f.senha =:senha", Funcionario.class);
+            query.setParameter("email", email);
+            query.setParameter("senha", senha);
+            return query.getSingleResult();
+        }
+        catch(NoResultException e){
+            throw new AuthenticationException("Login ou senha incorretos");
+        }
+        
     }
     
 }
